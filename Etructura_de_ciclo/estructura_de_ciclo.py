02109ddccadc87213fc1b4794fbabe43d5db93f9
zarahi <llamoccaortizk@gmail.com>
2022-10-12 12:39:16 -0500

    mistrabajos miercoles

--- Etructura_de_ciclo/estructura_de_ciclo.py
+++ Etructura_de_ciclo/estructura_de_ciclo.py
@@ -6,22 +6,37 @@
 ##while tambien es una estructura de ciclo
 ##int es metodo de python quien transforma un dato tipo texto a un dayto de tipo numerico trel o entero
 ##input es un metodo de python que pide un dato por consola al usuario
-condicion=True
-while condicion==True:
-    numero=int(input('ingrsa el numero ganador :'))
-    if numero==10:
-        print('ganaste el premio')
-        condiciones=False
-    else:
-        print('ese no es el numero ganador')
+##condicion=True
+##while condicion==True:
+   ## numero=int(input('ingrsa el numero ganador :'))
+   ## if numero==10:
+       ## print('ganaste el premio')
+        ##condiciones=False
+   ## else:
+    ##    print('ese no es el numero ganador')
 ##TAREA:
 ##CREAR UN PROGRAMA QUE ME PIDA MI EDAD, SI INGRESO UNA EDAD INCORRECTA EL PROGRAMA SEGUIRA PIDIENDO MI EDAD
 ##SI ES LA EDAD CORRECTA ME MOSTRARA UN MENSAJE DE CORRECTO Y SE TERMINA LA EJECUSIONES
-condicion=True
-while condicion==True:
-    edad=int(input('ingresa la edad:'))
-    if edad==16:
-        print('la edad es correcta')
-        condiciones=False
+##condicion=True
+#while condicion==True:
+ ##   edad=int(input('ingresa la edad:'))
+ ##   if edad==16:
+  ##      print('la edad es correcta')
+  ##      condiciones=False
+  ##  else:
+  ##      print('la edad no es correcta')
+##
+password='72884335'
+for intentos in range(1,4):
+    print('este es tu',intentos,'intento')
+    newpassword=input("ingresa el password correcto:")
+    if newpassword==password:
+        print("bienvenidos al sistema joven")
+        break
     else:
-        print('la edad no es correcta')
+        print("password incorrecto sigue intentando")
+
+
+
+
+
--- funciones/funciones.py
+++ funciones/funciones.py
@@ -4,4 +4,35 @@
 ##Dividir y organaizar el codigo en partes mas sencillas.
 ##Encapsular el codigo que se repite a lo largo de un programa para ser reutilizado.
 ##SINTAXIS:
-##La palabra clave
+##La palabra clave def
+##Un nobre de funcion
+##Prentesis() y dentro de los parentesisi los parameyros de entrada aunque los parametros de entrada sean opcionales
+##dos puntos":"
+##Algun bloque de codigo ´para ejecutar
+##Una sentencia de retorno
+##2 tipos de funciones
+##funciones propias de python
+print()##sirve para mostrar mensaje o resultado
+int()##sirve para convertir String en entero
+input()##esta funcion sirve para pedir datos al usuario
+##todo lo que se ingrese en imput sera tomado como texto
+##funciones creadas:
+##estructura para crear funciones
+def saludo()
+    return "hola"
+##uso de funcion
+print(saludo())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
