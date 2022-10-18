b422f430426808344add3cf13167808bf5203f5d
zarahi <llamoccaortizk@gmail.com>
2022-10-18 13:15:07 -0500

    clases hoy martes

--- Etructura_de_ciclo/estructura_de_ciclo.py
+++ Etructura_de_ciclo/estructura_de_ciclo.py
@@ -3,7 +3,7 @@
 
 #for contador in rangue
 
-##while tambien es una estructura de ciclo
+##(while tambien es una estructura de ciclo
 ##int es metodo de python quien transforma un dato tipo texto a un dayto de tipo numerico trel o entero
 ##input es un metodo de python que pide un dato por consola al usuario
 ##condicion=True
@@ -26,25 +26,61 @@
   ##  else:
   ##      print('la edad no es correcta')
 ##
-password='72884335'
-for intentos in range(1,4):
-    print('este es tu',intentos,'intento')
-    newpassword=input("ingresa el password correcto:")
-    if newpassword==password:
-        print("bienvenidos al sistema joven")
-        break
-    else:
-        print("password incorrecto sigue intentando")}
+##password='72884335'
+##for intentos in range(1,4):
+##    print('este es tu',intentos,'intento')
+##    newpassword=input("ingresa el password correcto:")
+##    if newpassword==password:
+##        print("bienvenidos al sistema joven")
+##        break
+##    else:
+##        print("password incorrecto sigue intentando")
 ##
-condicion=True
-eval=1
-while condicion==True:
-    if eval==5:
-        print("estamos en 5")
-        condicion=False
-    else:
-        print(eval)
-        eval+=1
+##condicion=True
+##eval=1
+##while condicion==True:
+##    if eval==5:
+##        print("estamos en 5")
+##        condicion=False
+##    else:
+##        print(eval)
+##       eval+=1
+##tabla de multiplicar
+##mostrar la tabla del numero que quieras
+##numero=int(input("ingrese un numero npara mostrar la tabla qhe quiere visualizar"))
+##for numeros in range(1,11):
+##    print(numeros,"*",numero,"=",numero*numero)
+##evaluando:
+##eval=True
+#while eval==True:
+#    numero=int(input('ingrese un numero para mostrar la tabla he quiere visualizar:'))
+#    if numero==0:
+#        print("error saliendo del programa")
+#        break
+#    else:
+#        for numeros in range(1, 11):
+#            print(numeros, "*", numero,"=",numero * numero)
+
+#mensaje ="hola"
+#print(mensaje[3])
+#for letra in mensaje:
+#    print(letra)
+#ingresarv un mensaje que
+#mostrar por consola cuantas vocales "a"
+#tiene el mensaje
+
+
+#mensajes =input("ingrese un mensaje")
+#contador=0
+#for letras in mensajes:
+#    if letras=="a":
+#      contador+=1
+#print("en este mensaje tienes",contador,"letras a")
+
+
+
+
+
 
 
 
